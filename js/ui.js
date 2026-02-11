eae3e863f01b0c0b210d552057317452d9abfacb
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-02-11 10:45:50 +0545

    filter logic added by category and minimum amount

--- js/ui.js
+++ js/ui.js
@@ -9,6 +9,10 @@ const totalAmount = document.querySelector(".total-amount");
 const expenseList = document.querySelector(".expense-list ul");
 const emptyList = document.querySelector(".empty-state");
 
+const filterCategory = document.querySelector("#filter-category");
+const filterAmount = document.querySelector("#filter-amount");
+const resetBtn = document.querySelector("#reset-filter");
+
 function getTotal(expenses){
     let total = 0;
     for(let i=0; i<expenses.length;i++){
@@ -59,3 +63,49 @@ function renderExpenses(expenses){
     }
 }
 
+
+
+filterCategory.addEventListener("change",function(){
+    const filteredExpenses = filterByCategory(expenses);
+    renderExpenses(filteredExpenses);
+})
+function filterByCategory(expenses){
+    let filterExpenses=[];
+    if(filterCategory.value==="all"){
+        filterExpenses = expenses;
+    }
+    else{
+        for(let i=0;i<expenses.length;i++){
+            if(expenses[i].category === filterCategory.value){
+                filterExpenses.push(expenses[i]);
+            }
+        }
+    }
+    return filterExpenses;
+}
+
+function filterByAmount(expenses){
+        let filterExpenses = [];
+        const minAmount = Number(filterAmount.value);
+        if(minAmount===0){
+            filterExpenses = expenses;
+        }else{
+            for(let i=0;i<expenses.length;i++){
+                if(expenses[i].amount<=minAmount){
+                    filterExpenses.push(expenses[i]);
+                }
+            }          
+        }
+        return filterExpenses;
+}
+
+filterAmount.addEventListener("input",function(){
+    const filtered = filterByAmount(expenses);
+    renderExpenses(filtered);
+})
+
+resetBtn.addEventListener("click",function(){
+    filterCategory.value="all";
+    filterAmount.value="";
+    renderExpenses(expenses);
+})
